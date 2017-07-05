3dbabae2aa25d3b73a61de51a823acb3b0d7aac7
DaniVSainz <danivsainz@gmail.com>
2017-07-05 12:30:07 -0400

    Fix issue with not navigating to projects page, issue was double declaring it inside pages mdule and projects module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,11 +6,10 @@ import { NgaModule } from '../theme/nga.module';
 import { AppTranslationModule } from '../app.translation.module';
 
 import { Pages } from './pages.component';
-import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   imports: [CommonModule, AppTranslationModule, NgaModule, routing],
-  declarations: [Pages, ProjectsComponent]
+  declarations: [Pages]
 })
 export class PagesModule {
 }
