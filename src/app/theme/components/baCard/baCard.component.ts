564a9edffb37ecfb3435aba364aa8db4b8bccb56
danivsainz <danivsainz@gmail.com>
2018-04-05 14:22:55 -0400

    Add css class for my-card-header and login in angular component

--- src/app/theme/components/baCard/baCard.component.ts
+++ src/app/theme/components/baCard/baCard.component.ts
@@ -5,10 +5,12 @@ import {BaCardBlur} from './baCardBlur.directive';
 @Component({
   selector: 'ba-card',
   templateUrl: './baCard.html',
+  styleUrls: ['./baCard.component.scss']
 })
 export class BaCard {
   @Input() title:String;
   @Input() baCardClass:String;
   @Input() cardType:String;
   @Input() href:String
+  @Input() myCard:String;
 }
