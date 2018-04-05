894ff07c0215eaea35d9c169691b6543dbfc2b8b
danivsainz <danivsainz@gmail.com>
2018-04-05 12:49:28 -0400

    change github icon size

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -18,10 +18,7 @@ import { PAGES_MENU } from './pages.menu';
     <footer class="al-footer clearfix">
       <div class="al-footer-main clearfix">
         <ul class="al-share clearfix">
-          <li><i class="socicon socicon-facebook"></i></li>
-          <li><i class="socicon socicon-twitter"></i></li>
-          <li><i class="socicon socicon-google"></i></li>
-          <li><i class="socicon socicon-github"></i></li>
+          <li><a href="https://github.com/DaniVSainz"  target="_blank"><i class="socicon socicon-github"></i></ a></li>
         </ul>
       </div>
     </footer>
